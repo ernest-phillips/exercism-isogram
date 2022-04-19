ecd4b39b8a7a9b9fe91ac100914536c9968a15d9
Ernest <ernestlphillips@gmail.com>
2022-04-18 17:12:57 -0700

    starter

--- isogram.rb
+++ isogram.rb
@@ -5,14 +5,3 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
 =end
-class Isogram
-  def self.isogram?(input)
-    for n in input 
-      if input[n].count > 1
-        "Expected true, '#{input}' is an isogram"
-      else
-        "Expected false, '#{input}' is not an isogram"
-      end
-    end
-  end
-end
